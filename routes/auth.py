441977d956dc7e848c4794bbadde36d2e1c8e141
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2025-04-06 23:14:07 +0800

    Trigger user registration email

--- routes/auth.py
+++ routes/auth.py
@@ -1,10 +1,13 @@
+import json
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import schemas
-from repository import account as crud
+from aws.sqs import send_message
 from db.database import get_db
+from repository import account as crud
 
 router = APIRouter()
 
@@ -15,11 +18,25 @@ async def register(user: schemas.User, db: AsyncSession = Depends(get_db)):
   if existing_user:
     raise HTTPException(status_code=400, detail="User already exists")
 
-  new_user = await crud.create_user(db, user)
-  return JSONResponse(
-    status_code=201,
-    content={"message": "User registered successfully", "user": new_user.email}
-  )
+  try:
+    new_user = await crud.create_user(db, user)
+
+    if not new_user:
+      raise HTTPException(status_code=400, detail="Failed to create user")
+
+    user_data = {
+      "first_name": user.first_name,
+      "last_name": user.last_name,
+      "email": user.email,
+    }
+    await send_message(json.dumps(user_data), "register-user-event", "register")
+
+    return JSONResponse(
+      status_code=201,
+      content={"message": "User registered successfully", "user": new_user.email}
+    )
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 @router.post("/login")
