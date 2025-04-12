9f950a08e2502895d770ad14b4071d419bb12838
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2025-04-12 19:34:56 +0800

    add Valkey container support and enhance Docker scripts

--- scripts/down.sh
+++ scripts/down.sh
@@ -3,8 +3,10 @@ set -e
 
 # Define container and image names
 DB_CONTAINER="account-mgr-db"
+VK_CONTAINER="account-mgr-vk"
 APP_IMAGE="queuetopia_account-account-mgr"
 DB_IMAGE="queuetopia_account-account-mgr-db"
+VK_IMAGE="queuetopia_account-account-mgr-vk"
 
 echo "Stopping and removing the Docker containers..."
 if command -v docker-compose &> /dev/null; then
@@ -17,17 +19,21 @@ fi
 FOUND_CONTAINER=$(docker ps -a --format '{{.Names}}' | grep "^$DB_CONTAINER$" || true)
 
 if [[ -n "$FOUND_CONTAINER" ]]; then
-    echo "Stopping and removing database container: $DB_CONTAINER..."
+    echo "Stopping and removing database container: $DB_CONTAINER & $VK_CONTAINER..."
     docker stop "$DB_CONTAINER"
     docker rm "$DB_CONTAINER"
     echo "✅ Removed container: $DB_CONTAINER"
+    docker stop "$VK_CONTAINER"
+    docker rm "$VK_CONTAINER"
+    echo "✅ Removed container: $VK_CONTAINER"
 else
-    echo "⚠️ Database container $DB_CONTAINER not found."
+    echo "⚠️ Database container $DB_CONTAINER OR $VK_CONTAINER not found."
 fi
 
 echo "Removing Docker images..."
 docker rmi -f "$APP_IMAGE" || echo "Image $APP_IMAGE not found."
 docker rmi -f "$DB_IMAGE" || echo "Image $DB_IMAGE not found."
+docker rmi -f "$VK_IMAGE" || echo "Image $VK_IMAGE not found."
 
 echo "Cleaning up unused Docker resources..."
 docker image prune -af --filter "label=project=queuetopia-account-mgr" || echo "No unused images to remove."
